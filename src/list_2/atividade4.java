b9c7b95517ce3a3e0ffc8462139b65d5d239bbee
Yasmin <yasmin.5126c@gmail.com>
2022-11-20 17:02:46 -0300

    Sorteio

--- src/list_2/atividade4.java
+++ src/list_2/atividade4.java
@@ -10,11 +10,13 @@ public class atividade4 {
 
         int aposta, numeroSorteado = sorteio.nextInt(6)+1;
 
-        System.out.print("Escolha um número entre 1 e 6: ");
+        System.out.print("Escolha um número: ");
         aposta = entrada.nextInt();
 
         if (aposta == numeroSorteado){
             System.out.printf("Numero Apostado %d %nNumero Sorteado %d %nGANHOU!!",aposta,numeroSorteado);
+        }else if(aposta > 6){
+            System.out.println("Erro! Por favor, escolha um número entre 1 e 6");
         }else {
             System.out.printf("Numero Apostado %d %nNumero Sorteado %d %nPERDEU",aposta,numeroSorteado);
         }
