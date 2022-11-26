eaba5e159ba8334ca7ba84c52569757a94001bed
Yasmin <yasmin.5126c@gmail.com>
2022-11-26 03:43:39 -0300

    Faça um programa que leia um número inteiro e mostre o seu antecessor e seu sucessor.

---
+++ src/Estudo_Sequencias_Basicas/Atv05NumeroSucessoEAntecessor.java
@@ -0,0 +1,9 @@
+package Estudo_Sequencias_Basicas;
+
+public class Atv05NumeroSucessoEAntecessor {
+    public static void main(String[] args) {
+        int numero = 9;
+        System.out.println("O antecessor de "+ numero + " é " + (numero-1));
+        System.out.println("O sucessor de "+ numero + " é " + (numero+1));
+    }
+}
