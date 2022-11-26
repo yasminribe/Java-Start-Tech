5dea9814a0eba7eb155793edebfa3ab4518565cc
Yasmin <yasmin.5126c@gmail.com>
2022-11-26 03:29:14 -0300

    Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas vindas para ela.

--- src/Estudo_Sequencias_Basicas/Atv01RespondendoAoUsuario.java
+++ src/Estudo_Sequencias_Basicas/Atv01RespondendoAoUsuario.java
@@ -3,6 +3,6 @@ package Estudo_Sequencias_Basicas;
 public class Atv01RespondendoAoUsuario {
     public static void main(String[] args) {
         String nome = "Lucas";
-        System.out.println("Seja bem vindo " + nome + "!!");
+        System.out.println("Seja bem vindo " + nome + "!");
     }
 }
