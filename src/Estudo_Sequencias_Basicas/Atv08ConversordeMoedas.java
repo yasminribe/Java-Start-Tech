aaa91d9d0d8c904c2a7159f40005ece73bcbaf0a
Yasmin <yasmin.5126c@gmail.com>
2022-11-26 19:18:27 -0300

    Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.

--- src/Estudo_Sequencias_Basicas/Atv08ConversordeMoedas.java
+++ src/Estudo_Sequencias_Basicas/Atv08ConversordeMoedas.java
@@ -2,9 +2,9 @@ package Estudo_Sequencias_Basicas;
 
 public class Atv08ConversordeMoedas {
     public static void main(String[] args) {
-        double dinheiro = 10;
-        double ValorDolar = 5.41;
+        double dinheiro = 125.90;
+        double ValoremDolar = dinheiro/5.41;
         System.out.printf("Seu saldo em carteira é R$ %.2f%n" +
-                "Valor convertido para Dolar U$ %.2f",dinheiro,(dinheiro/ValorDolar));
+                "Valor convertido para Dolar U$ %.2f",dinheiro,ValoremDolar);
     }
 }
