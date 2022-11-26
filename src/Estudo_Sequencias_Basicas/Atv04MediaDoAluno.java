e3123df66b340124c8d4d5357271df5dbc1d6b00
Yasmin <yasmin.5126c@gmail.com>
2022-11-26 03:41:51 -0300

    Faça um programa que leia as duas notas de um aluno em uma matéria e mostre na tela a sua média na disciplina.

---
+++ src/Estudo_Sequencias_Basicas/Atv04MediaDoAluno.java
@@ -0,0 +1,10 @@
+package Estudo_Sequencias_Basicas;
+
+public class Atv04MediaDoAluno {
+    public static void main(String[] args) {
+        double nota1 = 4.5;
+        double nota2 = 8.5;
+        double media = (nota1 + nota2) / 2;
+        System.out.printf("A media entre %.1f e  %.1f é igual a  %.1f ",nota1,nota2, media);
+    }
+}
