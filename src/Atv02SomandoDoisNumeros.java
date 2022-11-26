750f3af1e9da838bd2855b111b21cc83fab12241
Yasmin <yasmin.5126c@gmail.com>
2022-11-26 03:32:39 -0300

    Crie um programa que leia dois números e mostre a soma entre eles.

---
+++ src/Atv02SomandoDoisNumeros.java
@@ -0,0 +1,8 @@
+public class Atv02SomandoDoisNumeros {
+    public static void main(String[] args) {
+        int numero1 = 8;
+        int numero2 = 12;
+        int soma = numero1 + numero2;
+        System.out.println("A soma é: " + soma);
+    }
+}
