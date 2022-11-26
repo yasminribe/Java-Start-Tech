f65b27bbdf4c4c8939fec48ea627161b7762a3c6
Yasmin <yasmin.5126c@gmail.com>
2022-11-26 03:37:58 -0300

    Crie um programa que leia o nome e o salário de um funcionário, mostrando no final uma mensagem.

---
+++ src/Estudo_Sequencias_Basicas/Atv03NomeESalario.java
@@ -0,0 +1,11 @@
+package Estudo_Sequencias_Basicas;
+
+public class Atv03NomeESalario {
+    public static void main(String[] args) {
+        String nome = "Ana Maria ";
+        double salario = 1985.69;
+        System.out.printf("Nome Funcionário(a): %s%n" +
+                "Salário do mês R$ %.2f",nome,
+                salario);
+    }
+}
