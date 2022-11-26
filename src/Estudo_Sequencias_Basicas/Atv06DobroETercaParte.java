7c41ae1cf17a0d77dc9e808691b20c0492854651
Yasmin <yasmin.5126c@gmail.com>
2022-11-26 03:45:50 -0300

    Crie um programa que leia um número real e mostre na tela o seu dobro e a sua terça parte.

---
+++ src/Estudo_Sequencias_Basicas/Atv06DobroETercaParte.java
@@ -0,0 +1,9 @@
+package Estudo_Sequencias_Basicas;
+
+public class Atv06DobroETercaParte {
+    public static void main(String[] args) {
+        double numero = 3.5;
+        System.out.println("O dobro de " + numero + " é " + (numero * 2));
+        System.out.println("A terça parte de " + numero + " é " + (numero / 3));
+    }
+}
