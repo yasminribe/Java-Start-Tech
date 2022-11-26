1566e0561ac4272e1854b0ceb8a7b7a6e0579fc4
Yasmin <yasmin.5126c@gmail.com>
2022-11-26 20:44:53 -0300

    Escreva um programa que pergunte a velocidade de um carro. Caso ultrapasse 80Km/h, exiba uma mensagem dizendo que o usuário foi multado. Nesse caso, exiba o valor da multa, cobrando R$5 por cada Km acima da velocidade permitida.

---
+++ src/Estudo_Condicionais/Atv01RadarEletronico.java
@@ -0,0 +1,20 @@
+package Estudo_Condicionais;
+
+import java.util.Scanner;
+
+public class Atv01RadarEletronico {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double velocidade;
+
+        System.out.print("Digite a velocidade do carro: ");
+        velocidade = scan.nextInt();
+        double valorMulta = (velocidade - 80) * 5;
+
+        if (velocidade > 80){
+            System.out.printf("Motorista acima do limite de velocidade%n" +
+                    "MULTADO --------- Valor da multa R$ %.2f ",valorMulta);
+        }
+
+    }
+}
