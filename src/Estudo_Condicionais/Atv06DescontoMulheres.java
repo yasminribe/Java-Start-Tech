d85ff06637492c5ed376a32fef5d59aa34257b75
Yasmin <yasmin.5126c@gmail.com>
2022-11-27 00:03:13 -0300

    Numa promoção exclusiva para o Dia da Mulher, uma loja quer dar descontos para todos, mas especialmente para mulheres. Faça um programa que leia nome,sexo e o valor das compras do cliente e calcule o preço com desconto. Sabendo que:  - Homens ganham 5% de desconto  - Mulheres ganham 13% de desconto

---
+++ src/Estudo_Condicionais/Atv06DescontoMulheres.java
@@ -0,0 +1,34 @@
+package Estudo_Condicionais;
+
+import java.util.Scanner;
+
+public class Atv06DescontoMulheres {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String nome,
+                sexo;
+        double valorCompra;
+        System.out.print("Digite o nome: ");
+        nome = scan.nextLine();
+        System.out.print("Digite o sexo (F) ou (M) : ");
+        sexo = scan.nextLine().toUpperCase();
+
+        if ((!sexo.equals("F")) && !sexo.equals("M")) {
+            System.out.println("ERRO opção invalida");
+        } else {
+            System.out.print("Digite o valor da compra R$ ");
+            valorCompra = scan.nextDouble();
+            double desconto;
+
+            if (sexo.equals("F")) {
+                desconto = valorCompra - (valorCompra * 13) / 100;
+                System.out.printf("Bem vinda %s%nValor total R$ %.2f %nO valor total com DESCONTO ESPECIAL " +
+                        "R$ %.2f", nome, valorCompra, desconto);
+            } else {
+                desconto = valorCompra - (valorCompra * 5) / 100;
+                System.out.printf("Bem vindo %s%nValor total R$ %.2f %nO valor total com DESCONTO " +
+                        "R$ %.2f", nome, valorCompra, desconto);
+            }
+        }
+    }
+}
