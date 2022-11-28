2a2a890a1693815ec2dfcc1678e509669f065e4a
Yasmin <yasmin.5126c@gmail.com>
2022-11-28 02:18:24 -0300

    Crie um programa que receba uma palavra e uma letra. O programa deve então imprimir quantas vezes a letra informada aparece na palavra.

---
+++ src/list_3/Atv05OcorrenciaLetras.java
@@ -0,0 +1,27 @@
+package list_3;
+
+import java.util.Scanner;
+
+public class Atv05OcorrenciaLetras {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int contador = 0;
+        System.out.print("Digite uma palavra: ");
+        String palavra = scan.nextLine().toLowerCase();
+
+        System.out.print("Digite uma letra: ");
+        String letra = scan.nextLine().toLowerCase();
+
+        if (letra.length() != 1) {
+            System.out.printf("É necessário informar apenas 1 letra, mas foram informada %d", letra.length());
+        } else {
+            for (int i = 0; i < palavra.length(); i++) {
+                if (letra.charAt(0) == palavra.charAt(i)) {
+                    contador++;
+                }
+            }
+            System.out.printf("A quantidade de letras %S, na palavra %s é %d", letra, palavra, contador);
+        }
+
+    }
+}
