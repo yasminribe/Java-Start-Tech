120ab3aac3f54a1a64883045c1b62b7ad8978329
Yasmin <yasmin.5126c@gmail.com>
2022-11-28 00:36:55 -0300

    Escreva um programa que receba um número inteiro maior que 0. Imprima todos os números ímpares menores ou iguais a esse número.

--- src/list_3/Atv1NumerosImpares.java
+++ src/list_3/Atv1NumerosImpares.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class Atv1NumerosImpares {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-    System.out.print("Digite um número inteiro maior que zero(0): ");
-    int numero = scan.nextInt();
-        for (int contador = 1; contador <= numero; contador +=2 ){
+        System.out.print("Digite um número inteiro maior que zero(0): ");
+        int numero = scan.nextInt();
+        for (int contador = 1; contador <= numero; contador += 2) {
             System.out.println(contador);
         }
     }
