2bcd1009b4e53d34f9406abc0f55eee2bfb3e9b9
Yasmin <yasmin.5126c@gmail.com>
2022-11-30 09:46:11 -0300

    Escreva um programa que receba diversos nomes de aluno. Esse programa deve sortear os alunos em grupos de 3 pessoas. Caso o número de nomes recebidos não seja múltiplo de 3, o programa deve exibir uma mensagem de erro.

---
+++ src/list_4/Atv02CriandoGruposAlunos.java
@@ -0,0 +1,36 @@
+package list_4;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Atv02CriandoGruposAlunos {
+    public static void main(String[] args) {
+        String[] alunos = {"Pedro", "Luan", "Leticia", "Gabriela", "Juan","Bruna","Vanessa","Gustavo","Matheus"};
+        int qntGrupos = alunos.length / 3;
+        String[][] grupos = new String[qntGrupos][3];
+
+        if (alunos.length % 3 != 0) {
+            System.out.println("Erro! Não foi possivel dividir em grupos de 3.");
+        } else {
+            System.out.println("Nome dos ALUNOS: " + Arrays.toString(alunos));
+            for (int c = 0; c < qntGrupos; c++) {
+                int alunosDoGrupo = 0;
+
+                while (alunosDoGrupo < 3) {
+                    Random random = new Random();
+                    int sorteio = random.nextInt(0, alunos.length);
+
+                    if (!alunos[sorteio].equals("")) {
+                        grupos[c][alunosDoGrupo] = alunos[sorteio];
+                        alunosDoGrupo++;
+                        alunos[sorteio] = "";
+                    }
+                }
+            }
+            for (int i = 0; i < grupos.length; i++) {
+                System.out.printf("Grupo %d:%n", i + 1);
+                System.out.printf("%s%n", Arrays.toString(grupos[i]));
+            }
+        }
+    }
+}
