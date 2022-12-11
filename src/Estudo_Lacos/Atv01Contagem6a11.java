e31b04d125f7fbbb4c77f6ca16b955ccae7bbb9c
Yasmin <yasmin.5126c@gmail.com>
2022-12-11 00:55:56 -0300

    Escreva um programa que mostre na tela a seguinte contagem: 6 7 8 9 10 11 Acabou!

---
+++ src/Estudo_Lacos/Atv01Contagem6a11.java
@@ -0,0 +1,12 @@
+package Estudo_Lacos;
+
+public class Atv01Contagem6a11 {
+    public static void main(String[] args) {
+        int contador = 6;
+        while (contador <= 11) {
+            System.out.print(contador + " ");
+            contador++;
+        }
+        System.out.print("ACABOU!");
+    }
+}
