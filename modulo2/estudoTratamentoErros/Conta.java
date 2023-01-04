bbe6dfd35fe55ccd9edf229a3906cc0127d03d8b
yasminribe <111367166+yasminribe@users.noreply.github.com>
2023-01-03 21:50:33 -0300

    -Como analisar o rastro de exceções, ou stacktrace. -Tratar exceções com os blocos try-catch. -Manipular uma exceção lançada dentro do bloco catch. -Tratar múltiplas exceções com mais de um bloco catch ou usando Multi-Catch utilizando o pipe (|).

---
+++ modulo2/estudoTratamentoErros/Conta.java
@@ -0,0 +1,7 @@
+package estudoTratamentoErros;
+
+public class Conta {
+    void deposita(){
+
+    }
+}
---
+++ modulo2/estudoTratamentoErros/Fluxo.java
@@ -0,0 +1,46 @@
+package estudoTratamentoErros;
+
+public class Fluxo {
+
+    public static void main(String[] args) {
+        System.out.println("Ini do main");
+        try {
+
+        metodo1();
+        }catch (ArithmeticException | NullPointerException e) {
+           String msg = e.getMessage(); //mostra a msg de erro que foi gerada
+            System.out.println("Exception"+ msg);
+//            e.printStackTrace(); mostra o rastro da pilha onde foi dado o erro
+//        }catch ( NullPointerException e){
+//            System.out.println("NullPointerException");
+      }
+     System.out.println("Fim do main");
+ }
+
+
+    private static void metodo1() {
+        System.out.println("Ini do metodo1");
+//        try {
+        metodo2();
+//        }catch (ArithmeticException e){
+//            System.out.println("ArithmeticException");
+//        }
+//        System.out.println("Fim do metodo1");
+    }
+
+    private static void metodo2() {
+        System.out.println("Ini do metodo2");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+            // try {
+//            int a = i / 0;
+//            }catch (ArithmeticException ex){
+//            System.out.println("ArithmeticException");
+//            }
+            Conta c = null;
+            c.deposita();
+        }
+        System.out.println("Fim do metodo2");
+    }
+}
+
